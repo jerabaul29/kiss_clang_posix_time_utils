b26e5bf7b1a5f93d9e238da30b7ad2ce3c683778
JRT <jean.rblt@gmail.com>
2021-12-06 16:52:23 +0100

    Add tests about start and end of year / month, revert to old posix to calendar

--- tests/test_posix_time_utils.cpp
+++ tests/test_posix_time_utils.cpp
@@ -27,6 +27,65 @@ TEST_CASE("is_leap_year"){
     REQUIRE( is_leap_year(2400) );
 }
 
+TEST_CASE("calendar_to_posix"){
+    // a number of specific test cases that we can go from calendar to posix
+    // since we check below that calendar to posix and posix to calendar are inverse operation of
+    // each other, that should be enough to fully validate both functions.
+
+    kiss_calendar_time working_calendar;
+    kiss_time_t working_time;
+
+    // at around my time
+    working_time = 1638795207;
+    working_calendar = {2021, 12, 6, 12, 53, 27};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // back to the 70s! :)
+    working_time = 40003264;
+    working_calendar = {1971, 4, 9, 0, 1, 4};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // around year 2000
+    working_time = 951782464;
+    working_calendar = {2000, 2, 29, 0, 1, 4};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // by 2190, I will likely be long dead
+    working_time = 6952953588;
+    working_calendar = {2190, 4, 30, 23, 59, 48};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // but who knows in 2090?
+    working_time = 3801738768;
+    working_calendar = {2090, 6, 21, 14, 32, 48};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // second second of a year
+    working_time = 1609459201;
+    working_calendar = {2021, 1, 1, 0, 0, 1};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // first second of a year
+    working_time = 1609459200;
+    working_calendar = {2021, 1, 1, 0, 0, 0};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // last second of a year
+    working_time = 1640995199;
+    working_calendar = {2021, 12, 31, 23, 59, 59};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // last second of a month
+    working_time = 1614556799;
+    working_calendar = {2021, 2, 28, 23, 59, 59};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+
+    // first second of a month
+    working_time = 1614556800;
+    working_calendar = {2021, 3, 1, 0, 0, 0};
+    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
+}
+
 // if we go posix_time -> calendar -> posix_time, do we get back to the same time?
 // TODO: check in the middle that valid calendar!!
 bool back_and_forth_is_equal(kiss_time_t const posix_time){
@@ -64,38 +123,4 @@ TEST_CASE("random_back_and_forth_posix_calendar_posix"){
     for (size_t i=0; i<n_cases; i++){
         REQUIRE( back_and_forth_is_equal(get_random_posix()) );
     }
-}
-
-TEST_CASE("calendar_to_posix"){
-    // a number of specific test cases that we can go from calendar to posix
-    // since we checked above that calendar to posix and posix to calendar are inverse operation of
-    // each other, that should be enough to fully validate both functions.
-
-    kiss_calendar_time working_calendar;
-    kiss_time_t working_time;
-
-    // at around my time
-    working_time = 1638795207;
-    working_calendar = {2021, 12, 6, 12, 53, 27};
-    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
-
-    // back to the 70s! :)
-    working_time = 40003264;
-    working_calendar = {1971, 4, 9, 0, 1, 4};
-    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
-
-    // around year 2000
-    working_time = 951782464;
-    working_calendar = {2000, 2, 29, 0, 1, 4};
-    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
-
-    // by 2190, I will likely be long dead
-    working_time = 6952953588;
-    working_calendar = {2190, 4, 30, 23, 59, 48};
-    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
-
-    // but who knows in 2090?
-    working_time = 3801738768;
-    working_calendar = {2090, 6, 21, 14, 32, 48};
-    REQUIRE( calendar_to_posix(&working_calendar) == working_time );
 }
